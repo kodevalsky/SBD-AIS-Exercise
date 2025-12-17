96ee12bc2c8490c77a86b8180ac36346190571df
Jeremi Kowalski <jeremikow@gmail.com>
2025-12-17 14:39:24 +0100

    Finished exc_9

--- Exc_9/skeleton/main.go
+++ Exc_9/skeleton/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"exc9/mapred"
+	"fmt"
+	"os"
+	"strings"
 )
 
 // Main function
 func main() {
 	// todo read file
+	content, err := os.ReadFile("res/meditations.txt")
+	if err != nil {
+		panic(err)
+	}
+	text := strings.Split(string(content), "\n")
+
 	// todo run your mapreduce algorithm
 	var mr mapred.MapReduce
 	results := mr.Run(text)
 	// todo print your result to stdout
+	fmt.Println(results)
 }
--- Exc_9/skeleton/mapred/map_reduce.go
+++ Exc_9/skeleton/mapred/map_reduce.go
@@ -1,6 +1,78 @@
 package mapred
 
+import (
+	"regexp"
+	"strings"
+	"sync"
+)
+
 type MapReduce struct {
 }
 
 // todo implement mapreduce
+func (mr *MapReduce) Run(input []string) map[string]int {
+	// Map Phase
+	mapResults := make(chan []KeyValue, len(input))
+	var wg sync.WaitGroup
+
+	for _, line := range input {
+		wg.Add(1)
+		go func(l string) {
+			defer wg.Done()
+			mapResults <- mr.wordCountMapper(l)
+		}(line)
+	}
+
+	wg.Wait()
+	close(mapResults)
+
+	// Shuffle Phase
+	shuffled := make(map[string][]int)
+	for res := range mapResults {
+		for _, kv := range res {
+			shuffled[kv.Key] = append(shuffled[kv.Key], kv.Value)
+		}
+	}
+
+	// Reduce Phase
+	reduceResults := make(chan KeyValue, len(shuffled))
+	var wgReduce sync.WaitGroup
+
+	for key, values := range shuffled {
+		wgReduce.Add(1)
+		go func(k string, v []int) {
+			defer wgReduce.Done()
+			reduceResults <- mr.wordCountReducer(k, v)
+		}(key, values)
+	}
+
+	wgReduce.Wait()
+	close(reduceResults)
+
+	finalResult := make(map[string]int)
+	for res := range reduceResults {
+		finalResult[res.Key] = res.Value
+	}
+
+	return finalResult
+}
+
+func (mr *MapReduce) wordCountMapper(text string) []KeyValue {
+	reg, _ := regexp.Compile("[^a-zA-Z]+")
+	processedString := reg.ReplaceAllString(strings.ToLower(text), " ")
+	words := strings.Fields(processedString)
+
+	var results []KeyValue
+	for _, word := range words {
+		results = append(results, KeyValue{Key: word, Value: 1})
+	}
+	return results
+}
+
+func (mr *MapReduce) wordCountReducer(key string, values []int) KeyValue {
+	count := 0
+	for _, v := range values {
+		count += v
+	}
+	return KeyValue{Key: key, Value: count}
+}
